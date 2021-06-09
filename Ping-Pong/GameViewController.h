6eeaa0e9119aa353f3fe5d2de92528f494f712c3
Сергей Горячев <serezha_goryachev@list.ru>
2021-06-10 01:12:21 +0400

    Выполнил практическое задание по 7 уроку, переписал игру Ping-Pong с разделением основного метода config на несколько методов

---
+++ Ping-Pong/GameViewController.h
@@ -0,0 +1,22 @@
+//
+//  GameViewController.h
+//  Ping-Pong
+//
+//  Created by Сергей Горячев on 27.05.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+#define SCREEN_WIDTH [UIScreen mainScreen].bounds.size.width
+#define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
+#define HALF_SCREEN_WIDTH SCREEN_WIDTH/2
+#define HALF_SCREEN_HEIGHT SCREEN_HEIGHT/2
+#define MAX_SCORE 6
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GameViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
