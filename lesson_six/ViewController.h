dd5ae9f3f832bfc7ea62e2c438d93435f476a7a2
Сергей Горячев <serezha_goryachev@list.ru>
2021-06-10 01:05:59 +0400

    Выполнил практическое задание по 6 уроку, написал вывод списка студентов

---
+++ lesson_six/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  lesson_six
+//
+//  Created by Сергей Горячев on 14.05.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
